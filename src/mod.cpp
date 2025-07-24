0c7bac1799c0bdf996416ce797b653be48413813
jrad <215300829+jradcode23@users.noreply.github.com>
2025-07-23 18:22:14 -0700

    Updated mod.cpp to stay connected

--- src/mod.cpp
+++ src/mod.cpp
@@ -8,7 +8,16 @@
 int main() {
     std::cout << "Attempting initial connect" << std::endl;
     AP_Init("archipelago.gg:34565", "Manual_LegoBatmanTheVideoGame_SnolidIce", "Player1","");
-    
+    AP_SetItemClearCallback([](){
+        std::cout << "Calling ItemClearCallback Function" << std::endl;
+    });
+
+    AP_SetItemRecvCallback([&](int64_t itemID,bool notify){
+        itemID = 0;
+        notify = true;
+        std::cout << "Calling SetItemRecvCallback" << std::endl;
+    });
+    //AP_SetLocationInfoCallback();
     AP_Start();
     while(true){};
     return 0;
